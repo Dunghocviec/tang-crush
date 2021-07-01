4dbd24cba80f5b116bdf0e4b52a4c5db9abca5f0
Dunghocviec <86790425+Dunghocviec@users.noreply.github.com>
2021-07-01 22:32:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,13 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Chào Huyền😀'
+    introDesc: Giải lao tí nhé😁
+        
+ 
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    title: 'Dũng tò mò:)',
+    desc: ' M có thể nói cho t thông điệp của số 4 hm qua ko:>',
+    btnYes: 'Có',
+    btnNo: 'Không:3',
+    question:'nó là gì đấy? ',
+    btnReply: 'đc rồi',
+    messLink: 'https://m.me/beti.nhism',
